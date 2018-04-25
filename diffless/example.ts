2133dc37f594faad262b04e7de266892489829b8
Ygor Mutti <ygormutti@gmail.com>
2018-04-25 14:03:39 -0300

    Commented out examples to fix compilation

---
+++ diffless/example.ts
@@ -0,0 +1,16 @@
+// // declare var diffless;
+// declare var ZipSupport;
+// declare var PblSupport;
+// declare var JsonSupport;
+
+// const builder = new diffless.Builder();
+
+// // builder.addArchiveSupport(new ZipSupport());
+// // builder.addArchiveSupport(new PblSupport());
+
+// builder.addLanguageSupport(new JsonSupport());
+
+// const tool = builder.build();
+
+// const result = tool.compare("ab", "ababab");
+
