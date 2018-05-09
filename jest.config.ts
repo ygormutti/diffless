aff9fb43d83e3ab8ec116f4bddbc97f8488d945d
Ygor Mutti <ygormutti@gmail.com>
2018-05-09 10:25:22 -0300

    Move jest config to ts file

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export const transform = {
+    '^.+\\.tsx?$': 'ts-jest',
+};
+
+export const testRegex = '(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$';
+
+export const moduleFileExtensions = [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node',
+];
