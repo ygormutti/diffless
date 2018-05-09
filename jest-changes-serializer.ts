1867d2a27c6dea7eb760e1dc5692cdb595dc2d9a
Ygor Mutti <ygormutti@gmail.com>
2018-05-09 11:37:54 -0300

    Sort changes in serializer

--- jest-changes-serializer.ts
+++ jest-changes-serializer.ts
@@ -5,6 +5,13 @@ export function test(val: any): val is Change[] {
 }
 
 export function print(changes: Change[], serialize: (obj: any) => string): string {
+    changes.sort((a: Change, b:Change) =>{
+       let value = a.level - b.level;
+       if (!value) {
+           value = a.type - b.type;
+       }
+       return value;
+    });
     const objects = changes.map(c => ({
         ...c,
         level: ChangeLevel[c.level],
