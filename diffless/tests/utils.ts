1204b24aa60d8671f7e050cce0deefee9afd2c12
Ygor Mutti <ygormutti@gmail.com>
2018-06-10 22:04:31 -0300

    Test intEnumKeys

---
+++ diffless/tests/utils.ts
@@ -0,0 +1,16 @@
+import { ChangeLevel } from '../model';
+import { intEnumKeys } from '../util';
+
+enum IntEnum {
+    Zero,
+    One,
+    Two,
+}
+
+describe('utils', () => {
+    describe('intEnumKeys', () => {
+        it('should get enum keys correctly', () => {
+            expect(intEnumKeys(IntEnum)).toEqual(['Zero', 'One', 'Two']);
+        });
+    });
+});
