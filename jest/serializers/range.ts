832f71ca9b9cac17f06abc24c4a303a4fde9c18c
Ygor Mutti <ygormutti@gmail.com>
2018-06-29 23:06:43 -0300

    No unused parameters

--- jest/serializers/range.ts
+++ jest/serializers/range.ts
@@ -4,6 +4,6 @@ export function test(val: any): val is Range {
     return val && val instanceof Range;
 }
 
-export function print(range: Range, serialize: (obj: any) => string): string {
+export function print(range: Range): string {
     return range.toString();
 }
