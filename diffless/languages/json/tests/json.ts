3ae6c9244ba0543b0bed6d4244f7b581e895ace2
Ygor Mutti <ygormutti@gmail.com>
2018-09-09 22:44:45 -0300

    Fix code style

--- diffless/languages/json/tests/json.ts
+++ diffless/languages/json/tests/json.ts
@@ -2,24 +2,24 @@ import { announceHtml, fixtureDocument } from '../../../tests/test-util';
 import { jsonDiff, jsonLexicalDiff } from '../index';
 
 describe('JSON support', () => {
-    const mapReorderleft = fixtureDocument('single_map_reorder/before.json');
+    const mapReorderLeft = fixtureDocument('single_map_reorder/before.json');
     const mapReorderRight = fixtureDocument('single_map_reorder/after.json');
 
     describe('jsonLexicalDiff', () => {
         it('should match snapshot', () => {
-            const diff = jsonLexicalDiff(mapReorderleft, mapReorderRight);
+            const diff = jsonLexicalDiff(mapReorderLeft, mapReorderRight);
 
             expect(diff.edits).toMatchSnapshot();
-            announceHtml(mapReorderleft, mapReorderRight, diff.edits, 'json_lexical_1');
+            announceHtml(mapReorderLeft, mapReorderRight, diff.edits, 'json_lexical_1');
         });
     });
 
     describe('jsonDiff', () => {
         it('should match snapshot', () => {
-            const diff = jsonDiff(mapReorderleft, mapReorderRight);
+            const diff = jsonDiff(mapReorderLeft, mapReorderRight);
 
             expect(diff.edits).toMatchSnapshot();
-            announceHtml(mapReorderleft, mapReorderRight, diff.edits, 'json_1');
+            announceHtml(mapReorderLeft, mapReorderRight, diff.edits, 'json_1');
         });
 
         it('should match snapshot', () => {
