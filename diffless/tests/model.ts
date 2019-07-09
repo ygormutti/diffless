32ca6aec17cb117f18cc47e83a1d3e8cd26ad207
Ygor Mutti <ygormutti@gmail.com>
2019-07-08 21:38:51 -0300

    Fix typo in test descriptions

--- diffless/tests/model.ts
+++ diffless/tests/model.ts
@@ -10,27 +10,27 @@ describe('model', () => {
             expect(document).toMatchSnapshot();
         });
 
-        it('shoud get empty ranges correctly', () => {
+        it('should get empty ranges correctly', () => {
             const range = new Range(new Position(1, 1), new Position(1, 1));
             expect(document.getRange(range)).toBe('');
         });
 
-        it('shoud get end of line correctly', () => {
+        it('should get end of line correctly', () => {
             const range = new Range(new Position(1, 3), new Position(2, 1));
             expect(document.getRange(range)).toBe('\n');
         });
 
-        it('shoud get one line ranges correctly', () => {
+        it('should get one line ranges correctly', () => {
             const range = new Range(new Position(1, 1), new Position(1, 3));
             expect(document.getRange(range)).toBe('ac');
         });
 
-        it('shoud get two line ranges correctly', () => {
+        it('should get two line ranges correctly', () => {
             const range = new Range(new Position(1, 1), new Position(2, 2));
             expect(document.getRange(range)).toBe('ac\nd');
         });
 
-        it('shoud get multiline ranges correctly', () => {
+        it('should get multiline ranges correctly', () => {
             const range = new Range(new Position(1, 1), new Position(3, 1));
             expect(document.getRange(range)).toBe('ac\ndc\n');
         });
